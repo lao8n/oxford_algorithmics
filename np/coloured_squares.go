287fecb692fb80faadaaf9a6e8632ba9bf633f72
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2023-10-29 20:52:23 +0000

    starting to work on outside definition

--- np/coloured_squares.go
+++ np/coloured_squares.go
@@ -10,7 +10,30 @@ func main() {
 }
 
 type board struct {
+	n         int
 	locations [][]*square
+	edge      string
+}
+
+func (b *board) outside(x int, y int) string {
+	xy := []int{-1, 0, 1, 0, -1} // top-right-bottom-left
+	outside := ""
+	for i := 1; i < len(xy); i++ {
+		nx, ny := x+xy[i-1], y+xy[i]
+		// new coordinates are off board so needs to match edge
+		if nx < 0 || nx >= b.n || ny < 0 || ny >= b.n {
+			outside = outside + b.edge
+		} else { // on board so get value
+			// if not yet set can be anything
+			if b.locations[nx][ny] != nil {
+				outside += "*"
+			} else {
+				l := b.locations[nx][ny]
+				l.colours[l.rotation]
+			}
+		}
+	}
+	return outside
 }
 
 // string are always in the order top-right-bottom-left
